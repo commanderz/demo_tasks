139713f8d71fe71379e808cfa2d4a71e6bdec108
commanderz <supercommander@ukr.net>
2022-01-11 20:17:01 +0200

    no message

--- task2-todolist-dnd/src/List.tsx
+++ task2-todolist-dnd/src/List.tsx
@@ -8,6 +8,11 @@ enum eTaskStage {//етап виконання: Створено, Заплано
     tsPlaned = 'Заплановані справи',
     tsFinished = 'Завершені справи'
 }
+const id2List = {
+    droppable: 'created',
+    droppable2: 'planed',
+    droppable3: 'finished'
+};
 
 interface iTaskType {
     key: string;
@@ -17,6 +22,12 @@ interface iTaskType {
     taskStage: eTaskStage | null;//етап виконання: Створено, Заплановано, Завершено
 }
 
+interface iTasks {
+    created: iTaskType[];
+    planed: iTaskType[];
+    finished: iTaskType[];
+}
+
 interface iInputData {
     value: string;
     onChange: any;
@@ -24,18 +35,19 @@ interface iInputData {
 };
 const grid: number = 8;
 const QuoteItem: StyledComponent<any, any, any> = styled.div`
-  width: 200px;
-  height: 50px;
+  width: auto;
+  height: auto;
   border: 1px solid grey;
   margin-bottom: ${grid}px;
   padding: ${grid}px;
-  background-color:  lightblue;
+  background-color: lightblue;
   `;
 
 
 const STORAGE_KEY_TODOLIST: string = 'TodoList';
 const STORAGE_KEY_FORM: string = 'TodoForm';
 const EMPTY_FORM: iTaskType = { key: '', id: '', taskName: '', taskType: '', taskStage: null };
+const EMPTY_TABLE: iTasks = { created: [], finished: [], planed: [] };
 const generateId = () => new Date().getTime().toString();
 //const todolist: iTaskType[] = [];
 
@@ -43,12 +55,12 @@ const generateId = () => new Date().getTime().toString();
 //====================================================================================================================
 function List() {
     const [formValues, setFormValues] = useState<iTaskType>(EMPTY_FORM)
-    const [tableValues, setTableValues] = useState<iTaskType[]>([]);
+    const [tableValues, setTableValues] = useState<iTasks>(loadTask());
 
     const handleAddTodo: React.MouseEventHandler = (e: React.MouseEvent) => {
         //console.log('eventClick');
         if (!!formValues.id) {
-            tableValues.push(formValues);
+            tableValues?.created.push(formValues);
             setTableValues(tableValues);
             setFormValues(EMPTY_FORM);
 
@@ -65,18 +77,24 @@ function List() {
         const newForm: iTaskType = formString ? JSON.parse(formString) : EMPTY_FORM;
         setFormValues(newForm);
 
-        const tableString: string | null = localStorage.getItem(STORAGE_KEY_TODOLIST);
+        /*const tableString: string | null = localStorage.getItem(STORAGE_KEY_TODOLIST);
         const newTable: iTaskType[] = tableString ? JSON.parse(tableString) : [];
-        setTableValues(newTable);
+        setTableValues(newTable);*/
         //console.log('READ ' + STORAGE_KEY_USERS + ' STORAGE =' + newUsers?.length);
     }, []);
 
-
+    function loadTask(): iTasks {
+        if (window !== undefined) {//сторона клієнта
+            const tasksString: string | null = localStorage.getItem(STORAGE_KEY_TODOLIST);
+            const newTasks: iTasks = tasksString ? JSON.parse(tasksString) : [];
+            return (newTasks);
+        } else return EMPTY_TABLE;
+    }
 
     useEffect(() => { saveForm(formValues); }, [formValues]);
     useEffect(() => { saveTable(tableValues); }, [tableValues]);
     function saveForm(formData: iTaskType) { localStorage.setItem(STORAGE_KEY_FORM, JSON.stringify(formData)); };
-    function saveTable(tableData: iTaskType[]) { localStorage.setItem(STORAGE_KEY_TODOLIST, JSON.stringify(tableData)); };
+    function saveTable(tableData: iTasks) { localStorage.setItem(STORAGE_KEY_TODOLIST, JSON.stringify(tableData)); };
 
 
     const getItemStyle = (isDragging: boolean, draggableStyle: DraggingStyle | NotDraggingStyle | undefined) => ({
@@ -86,7 +104,7 @@ function List() {
         margin: `0 0 ${grid}px 0`,
 
         // change background colour if dragging
-        background: isDragging ? "lightgreen" : "grey",
+        background: isDragging ? "lightgreen" : null,
 
         // styles we need to apply on draggables
         ...draggableStyle
@@ -98,12 +116,40 @@ function List() {
         width: 250
     });
 
-    const reorder = (list: iTaskType[], startIndex: number, endIndex: number): iTaskType[] => {
+    /*const reorder = (list: iTaskType[], startIndex: number, endIndex: number): iTaskType[] => {
         const result: iTaskType[] = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
         result.splice(endIndex, 0, removed);
         return result;
+    };*/
+    function reorder2(list: iTaskType[], startIndex: number, endIndex: number): iTaskType[] {
+        const result: iTaskType[] = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+
     };
+
+    function getList(id: string): iTaskType[] {
+        if (id === 'droppable1') { return tableValues.created }
+        else
+            if (id === 'droppable2') { return tableValues.planed }
+            else
+                if (id === 'droppable3') { return tableValues.finished }
+                else return [];
+        //return state[id2List[id]]; 
+    };
+    function saveList(id: string, arr: iTaskType[]) {
+        if (id === 'droppable1') {
+            setTableValues({ created: arr, planed: tableValues.planed, finished: tableValues.finished });
+        } else
+            if (id === 'droppable2') {
+                setTableValues({ created: tableValues.created, planed: arr, finished: tableValues.finished });
+            } else
+                if (id === 'droppable3') {
+                    setTableValues({ created: tableValues.created, planed: tableValues.planed, finished: arr });
+                }
+    }
 
     function onDragEnd(r: DropResult): void {
         if (!r.destination) {
@@ -116,20 +162,16 @@ function List() {
         if (r.destination.droppableId === r.source.droppableId) {
             console.log('move src = ' + r.source.droppableId + ', idx=' + r.source.index);
             console.log('move dest = ' + r.destination.droppableId + ', idx=' + r.destination.index);
-            /*const quote = reorder(
+            const quote = reorder2(
                 //state.quotes,
                 getList(r.source.droppableId),
                 r.source.index,
                 r.destination.index
             );
-            console.log(quote);*/
-            /*if (r.source.droppableId === 'droppable') {
-                setState({ quotes: quote, selected: state.selected });
-            } else
-                if (r.source.droppableId === 'droppable2') {
-                    setState({ quotes: state.quotes, selected: quote });
-                }
-                */
+            console.log(quote);
+            saveList(r.source.droppableId, quote);
+
+
         } else {
             console.log('move2 src = ' + r.source.droppableId + ', idx=' + r.source.index);
             console.log('move2 dest = ' + r.destination.droppableId + ', idx=' + r.destination.index);
@@ -142,17 +184,18 @@ function List() {
         //const len = tableValues.length - 1;
         //console.log('numStage=' + p.numStage + ', cnt=' + len + ', name=' + todolist[len]?.taskName + ', type=' + todolist[len]?.taskType + ', stage=' + todolist[len]?.taskStage);
         let tableStage: eTaskStage | null = null;
-        if (p.numStage === 1) { tableStage = eTaskStage.tsCreated } else
-            if (p.numStage === 2) { tableStage = eTaskStage.tsPlaned } else
-                if (p.numStage === 3) { tableStage = eTaskStage.tsFinished }
+        let newlist: iTaskType[] = [];
+        if (p.numStage === 1) { tableStage = eTaskStage.tsCreated; newlist = tableValues.created; } else
+            if (p.numStage === 2) { tableStage = eTaskStage.tsPlaned; newlist = tableValues.planed; } else
+                if (p.numStage === 3) { tableStage = eTaskStage.tsFinished; newlist = tableValues.finished; }
 
 
 
-        const newlist: iTaskType[] = [];
-        tableValues.filter((value) => {
+
+        /*tableValues.filter((value) => {
             //console.log('arr=' + array.length + ' ts1=' + value.taskStage + ', ts2=' + tableStage);
             if (value.taskStage === tableStage) { newlist.push(value); }
-        });
+        });*/
 
         //console.log('new=' + newlist.length);
         //=========================================================================================================
@@ -163,10 +206,12 @@ function List() {
                 <div className="div0">
                     <Droppable droppableId={"droppable" + p.numStage} mode="standard" isDropDisabled={false} isCombineEnabled={false} direction="vertical" >
                         {(provided, snapshot) => (
-                            <div ref={provided.innerRef} {...provided.droppableProps} >
+                            <div ref={provided.innerRef} {...provided.droppableProps}
+                                style={getListStyle(snapshot.isDraggingOver)}
+                            >
                                 {provided.placeholder}
                                 {newlist.map((item: iTaskType, index: number) => (
-                                    <Draggable draggableId={item.id} index={index} key={item.id}>
+                                    <Draggable draggableId={item.id} index={index} key={item.id} disableInteractiveElementBlocking={true}>
                                         {(provided, snapshot) => (
                                             <QuoteItem className="divitem"
                                                 ref={provided.innerRef}
